16c4d1d1ffa46367718b3cac3adcbe25447b9c19
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-04-28 12:54:17 +0600

    [vite,config: dedicated vite config for lib;

---
+++ vite.config.lib.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    }
+  }
+})
