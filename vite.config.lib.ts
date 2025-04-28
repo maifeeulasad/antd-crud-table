cb29d773dddf0364952eb7dfdaa4c6c3311fb416
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-04-28 19:50:09 +0600

    [ts]: generate `.d.ts` files;

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      entryRoot: 'lib',
+      outDir: 'dist',
+      tsconfigPath: './tsconfig.build.json',
+      copyDtsFiles: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'lib/main.ts'),
