a602b1d56e3ef99c7fc8bba4a47fe53352dffab9
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-04-28 20:47:37 +0600

    [cleanup]: removed `lib/main.ts`;

--- lib/main.ts
+++
@@ -1,5 +0,0 @@
-import CrudTable from "./CrudTable";
-import CrudTableLazy from "./CrudTableLazy";
-
-export { CrudTable, CrudTableLazy };
-export type { CrudTableConfig, CrudColumn, DataType } from "./CrudTable";
